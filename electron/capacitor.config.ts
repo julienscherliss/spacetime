c28ea8e5a9f81f2e324bc22c5ff9cc1128effde7
Julien Scherliss <julienscherliss@gmail.com>
2026-04-17 10:31:12 -0700

    local changes

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.9e3e0ce65e59410282833dbdc7dde026',
+  appName: 'Spacetime',
+  webDir: 'dist',
+  server: {
+    // For local development with hot-reload, uncomment the url below:
+    // url: 'https://9e3e0ce6-5e59-4102-8283-3dbdc7dde026.lovableproject.com?forceHideBadge=true',
+    // cleartext: true,
+  },
+};
+
+export default config;
